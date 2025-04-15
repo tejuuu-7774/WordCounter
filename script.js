69247ad45d4e9e623c38e6e1c77dcdabf1bea2f1
Palwai Tejaswini <palwaitejaswini@Palwai-ka-MacBook-Pro.local>
2025-04-16 00:36:51 +0530

    made few changes in both script.js and style.css

--- script.js
+++ script.js
@@ -2,7 +2,9 @@ const textArea = document.getElementById('textArea');
 const Countword = document.getElementById('word');
 const Countchar = document.getElementById('char');
 
+// added an eventlistener to the input area
 textArea.addEventListener('input', () => {
+    // gives the current value status
     const text = textArea.value;
     Countchar.innerHTML = text.length;
 
